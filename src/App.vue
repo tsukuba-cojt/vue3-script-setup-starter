4b1d8eb64741c5685eefce51dd28f7704efc3c17
Hidetaro7 <hidetaro7@gmail.com>
2023-04-17 19:00:02 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="message" />
+    <div id="message">{{ message }}</div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const message = ref()
+</script>
+
+<style>
+#message {
+  background-color: #efefef;
+  padding: 1rem;
+  margin: 1rem 0;
+}
+</style>
